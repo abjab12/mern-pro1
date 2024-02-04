21bedbd0066184876462a929d36b947aeba2664a
abjab12 <abraham.sh124@gmail.com>
2024-02-03 17:48:35 -0800

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Signin from './pages/Signin';
+import About from './pages/about';
+import SignUp from './pages/signUp';
+import Profile from './pages/profile'; // Assuming you have a Profile component
 
 export default function App() {
   return (
-    <div>
-      app
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signUp" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
