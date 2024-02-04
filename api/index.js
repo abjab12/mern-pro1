95b89b2b4c1dc60315b89ff136174db33a4e677f
abjab12 <abraham.sh124@gmail.com>
2024-02-04 09:42:03 -0800

    connect db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
-import express  from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const mongoURI = process.env.MONGO;
+
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
 const app = express();
-app.listen(3000, () =>{
-    console.log("Server is running on port 3000!");
-}
-);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
